280ba2ba7e2ae7659afce01b77c8f8d82ea3d202
taofeek <taofeek01@yahoo.com>
2024-04-01 14:32:40 +0100

    multipage farcaster frame and mint

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,107 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput, parseEther } from "frog";
+import { handle } from "frog/vercel";
+import { devtools } from "frog/dev";
+import { serveStatic } from "frog/serve-static";
+import { getNftMetadata } from "@/app/lib/getNftMetadata";
+import { abi } from "@/app/lib/abi";
+
+const app = new Frog({
+  basePath: "/api",
+  // Supply a Hub API URL to enable frame verification.
+  hubApiUrl: "https://api.hub.wevm.dev",
+});
+
+const ShapesContractAddress = "0x488A5c5f0aA5f44C8438A79E17867b5d30C418b3";
+const maxSupply = 10;
+
+// Frame to display user's response.
+app.frame("/", (c) => {
+  return c.res({
+    image: "https://i.ibb.co/SmM9m2F/New.png",
+    imageOptions: { width: 600, height: 600 },
+    intents: [
+      <Button action="/view" value="1">
+        View Collection
+      </Button>,
+    ],
+  });
+});
+
+app.frame("/view", async (c) => {
+  const { buttonValue, status } = c;
+  const randomTokenId = Math.floor(Math.random() * maxSupply) + 1;
+
+  const nftMetadata = await getNftMetadata(ShapesContractAddress, randomTokenId);
+
+  const nftImageUrl = nftMetadata?.image?.cachedUrl;
+
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <img
+          src={nftImageUrl}
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            transform: "rotate(90deg)",
+          }}
+        />
+      </div>
+    ),
+    imageOptions: { width: 600, height: 600 },
+    intents: [
+      <Button value="1">More</Button>,
+      <Button.Transaction action="/finish" target="/mint">
+        Mint
+      </Button.Transaction>,
+    ],
+  });
+});
+
+app.transaction("/mint", (c) => {
+  // Contract transaction response.
+
+  return c.contract({
+    abi,
+    chainId: "eip155:8453",
+    functionName: "mint",
+    to: "0x488A5c5f0aA5f44C8438A79E17867b5d30C418b3",
+  });
+});
+
+app.frame("/finish", (c) => {
+  const { transactionId } = c;
+  return c.res({
+    image: "https://i.ibb.co/sP5phSQ/success.png",
+    imageOptions: { width: 600, height: 600 },
+
+    intents: [
+      <Button action="/view" value="1">
+        View Collection
+      </Button>,
+      ,
+      <Button.Link href={`https://basescan.org/tx/${transactionId}`}>BaseScan</Button.Link>,
+      <Button.Link href="https://opensea.io/collection/shapes-45">OpenSea</Button.Link>,
+      ,
+    ],
+  });
+});
+
+devtools(app, { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
---
+++ app/page.tsx
@@ -0,0 +1,55 @@
+import styles from "./page.module.css";
+
+import { getFrameMetadata } from "frog/next";
+import type { Metadata } from "next";
+import Link from "next/link";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const url = process.env.VERCEL_URL || "http://localhost:3000";
+  const frameMetadata = await getFrameMetadata(`${url}/api`);
+  return {
+    other: frameMetadata,
+  };
+}
+export default async function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <Link
+          href="https://opensea.io/collection/shapes-45"
+          style={{
+            background: "gold",
+            padding: "1rem",
+            borderRadius: "8px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "0.5rem",
+            color: "black",
+            fontWeight: "bold",
+            width: "11rem",
+          }}
+        >
+          OpenSea
+        </Link>
+        <Link
+          href="https://opensea.io/collection/shapes-45"
+          style={{
+            background: "gold",
+            padding: "1rem",
+            borderRadius: "8px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "0.5rem",
+            color: "black",
+            fontWeight: "bold",
+            width: "11rem",
+          }}
+        >
+          Mint on Farcaster
+        </Link>
+      </div>
+    </main>
+  );
+}
